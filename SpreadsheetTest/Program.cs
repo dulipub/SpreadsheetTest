a7c4e6d10dda1cdf268fe978aa557872c80455ba
dulipub <dulipub@gmail.com>
2024-01-29 09:00:29 +0530

    Appsettings and environment variables

--- SpreadsheetTest/Program.cs
+++ SpreadsheetTest/Program.cs
@@ -1,4 +1,15 @@
+using SpreadsheetTest;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Appsettings and .env
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+    .AddEnvironmentVariables();
+
+SetEnvironment.Load();
 
 // Add services to the container.
 
